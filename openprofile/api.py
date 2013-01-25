363cf2d511642b13f4b6fe25ff33d5fa6d2a4e97
Lorenzo Setale <koalalorenzo@gmail.com>
2013-01-25 23:38:25 +0100

    Considering openprofile.objects as package/sub-module

--- openprofile/api.py
+++ openprofile/api.py
@@ -2,7 +2,9 @@ from openprofile import app
 from openprofile.decorators import *
 import json
 
-@app.route("/api/contact")
+@app.route("/api/peers/contact")
 def api_contact():
-    # return the virtual business in json format
+    # return the openprofile business card in json format: used by other installations
+    # it contains also the key (or a sort of ) to encrypt messages. if this key changes
+    # the owner must re-authorize the other friend.
     json.dumps({})
