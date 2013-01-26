4e3b5654eaa76c8bbb9f0230c972401557308329
Lorenzo Setale <koalalorenzo@gmail.com>
2013-01-26 11:54:06 +0100

    Moved stupid data file in contents

--- openprofile/contents.py
+++ openprofile/contents.py
@@ -8,3 +8,18 @@ def index():
 @app.route("/<page>")
 def page(page):
     return "Hello World!"
+
+@app.route("/avatar")
+def avatar():
+    # return Avatar image in base of a format
+    return "Hello World!"
+
+@app.route("/card")
+def vcard():
+    # return the virtual business card in base of the specified format 
+    return "Hello World!"
+
+@app.route("/qrcode")
+def qrcode():
+    # return a qrcode image ( jpeg or png ) with  
+    return "Hello World!"
