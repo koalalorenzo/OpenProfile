6af3e821dddfeb2c2588b18d024e03b02036982d
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-10 13:24:00 +0100

    Created the view/controller to display Pages

--- openprofile/views/contents.py
+++ openprofile/views/contents.py
@@ -1,18 +1,31 @@
 from openprofile import app
 from openprofile.decorators import *
+
+from openprofile.objects import Page
+from openprofile.objects import Profile
+
 from random import choice
 
+from flask import Flask, render_template, session, url_for, abort, redirect, flash, request
+
+import markdown
+
 @app.route('/')
-@cached()
 def index():
     """Home page of the profile"""
     return 'Hello World!'
 
-@app.route("/<page>")
-@cached()
-def page(page):
+@app.route("/<page_url>")
+def page(page_url):
     """Generic Page View"""
-    return "Hello Page! %s" % choice([0,1,2,3,4,5,6,7,8,9]) 
+    profile = Profile()
+    profile.load_admin()
+    
+    page = Page("/%s" page_url)
+    page.load()
+    html = markdown.markdown(page.content)
+    
+    return render_template('page.html', page=page, html=html, profile=profile)
 
 @app.route("/avatar")
 def avatar():
