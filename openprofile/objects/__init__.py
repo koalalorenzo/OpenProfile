33385800c41b86480fe0466774e668e6d2fe93fc
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-10 20:17:02 +0100

    Loaded PageNotFound and AdminNotFound by default

--- openprofile/objects/__init__.py
+++ openprofile/objects/__init__.py
@@ -1,4 +1,4 @@
 from openprofile.objects.Connection import Connection
-from openprofile.objects.Page import Page
+from openprofile.objects.Page import Page, PageNotFound
 from openprofile.objects.Message import Message
-from openprofile.objects.Profile import Profile
+from openprofile.objects.Profile import Profile, AdminNotFound
