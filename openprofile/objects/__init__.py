4cb814fbe59b9e945a4ce81f8063e29a9f8ead9c
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-10 19:35:44 +0100

    Importing correctly the objects

---
+++ openprofile/objects/__init__.py
@@ -0,0 +1,4 @@
+from openprofile.objects.Connection import Connection
+from openprofile.objects.Page import Page
+from openprofile.objects.Message import Message
+from openprofile.objects.Profile import Profile
