12502d84ba46e916324bd946e5208df43ba4d0bc
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-18 19:48:02 +0100

    Created a new decorator to manage authentication

--- openprofile/decorators.py
+++ openprofile/decorators.py
@@ -1,4 +1,7 @@
 from flask import request
+from flask import session
+from flask import abort
+
 from werkzeug.contrib.cache import SimpleCache
 from functools import wraps
 
@@ -14,6 +17,18 @@ def cached(timeout=60*5, key='view/%s'):
                 return rv
             rv = f(*args, **kwargs)
             cache.set(cache_key, rv, timeout=timeout)
+            return rv
+        return decorated_function
+    return decorator
+
+def authorized():
+    def decorator(f):
+        @wraps(f)
+        def decorated_function(*args, **kwargs):
+            if not 'auth' in session:
+                session.pop('auth', None)
+                abort(403)
+            rv = f(*args, **kwargs)
             return rv
         return decorated_function
     return decorator
