a7cf142132627cf1a63614e69c0f6c01f6b1a527
Lorenzo Setale <koalalorenzo@gmail.com>
2013-02-18 19:44:43 +0100

    Improved API to get a specific profile.

--- openprofile/views/api.py
+++ openprofile/views/api.py
@@ -1,17 +1,24 @@
 from openprofile import app
+from openprofile import db
 from openprofile.decorators import *
 from openprofile.objects import Profile
 import json
 
-@app.route("/api/profile/owner")
-def api_contact():
+@app.route("/api/profile")
+@app.route("/api/profile/<userhash>")
+def api_show_profile(userhash=None):
     """
-        Returns the openprofile business card in json format: used by other installations
+        Returns the openprofile in json format: used by other installations
         it contains also the key (or a sort of ) to verify messages. If the key change,
         the owner must re-authorize the other friend.
     """
     profile = Profile()
-    profile.load_admin()
+    profile.database = db
+    if not userhash:
+        profile.load_admin()
+    else:
+        profile.userhash = userhash
+        profile.load()
     jsoned = profile.__dict__()
     del jsoned['is_admin']
     return json.dumps(jsoned)
